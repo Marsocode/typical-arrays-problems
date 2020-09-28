c50467f3406eeb7cd3b2f6ace7ee7298ed5cc844
Lika <borisovalika2@mail.com>
2020-09-29 01:03:45 +0300

    feat: min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
+// jshint esversion:6
 
 exports.min = function min (array) {
-  return 0;
-}
+	if (typeof array === 'undefined') return 0;
+  if (array.length === 0) return 0;
+  
+  let minValue = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < minValue) {
+      minValue = array[i];
+    }
+  }
+  return minValue;
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+  if (typeof array === 'undefined') return 0;
+  if (array.length === 0) return 0;
+
+  let maxValue = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > maxValue) {
+      maxValue = array[i];
+    }
+  }
+  return maxValue;
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+  if (typeof array === 'undefined') return 0;
+  if (array.length === 0) return 0;
+
+  let result = array.reduce((total, currentValue) => {
+    return (total + currentValue);
+  }, 0) / array.length;
+
+  return result;
+};
